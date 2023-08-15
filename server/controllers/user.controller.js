70cc611c1ba27fa303adcfea5123c53d294f95c8
Shresth Gour <akshat4575@gmail.com>
2023-08-16 00:11:47 +0530

    Added a functionality to upload images and to crop it

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,9 @@
 import User from "../models/user.model.js";
 import AppError from "../utils/error.util.js";
+import cloudinary from 'cloudinary';
+import fs from 'fs/promises'
+
+// 33:00
 
 const cookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
@@ -10,13 +14,13 @@ const cookieOptions = {
 const register = async (req, res, next) => {
   const { fullName, email, password } = req.body;
 
-  if(!fullName || !email || !password){
+  if (!fullName || !email || !password) {
     return next(new AppError('All fields are required', 400))
   }
 
   const userExists = await User.findOne({ email });
 
-  if(userExists){
+  if (userExists) {
     return next(new AppError('Email already exists!', 400));
   }
 
@@ -24,17 +28,41 @@ const register = async (req, res, next) => {
     fullName,
     email,
     password,
-    avatar:{
+    avatar: {
       public_id: email,
       secure_url: 'https://res.cloudinary.com/du9jzqlpt/image/upload/v1674647316/avatar_drzgxv.jpg'
     }
   })
 
-  if(!user) {
+  if (!user) {
     return next(new AppError('User registeration failed, please try again', 400))
   }
 
-  // TODO: File upload
+  console.log('File Details: ', JSON.stringify(req.file));
+  if (req.file) {
+    try {
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: 'lms',
+        width: 250,
+        height: 250,
+        gravity: 'faces',
+        crop: 'fill'
+      });
+
+      if (result) {
+        user.avatar.public_id = result.public_id;
+        user.avatar.secure_url = result.secure_url;
+
+        // Remove file from server
+        fs.rm(`uploads/${req.file.filename}`)
+
+      }
+    } catch (e) {
+      return next(
+        new AppError(e || 'File not uploaded, please try again', 500)
+      )
+    }
+  }
 
   await user.save()
 
@@ -55,24 +83,24 @@ const login = async (req, res) => {
 
   try {
     const { email, password } = req.body;
-  
-    if(!email || !password){
+
+    if (!email || !password) {
       return next(new AppError('All Fields are Required!', 400))
     }
-  
+
     const user = await User.findOne({
       email
     }).select('+password')
-  
-    if(!user || !user.comparePassword(password)){
+
+    if (!user || !user.comparePassword(password)) {
       return next(new AppError('Email or Password does not match!', 400))
     }
-  
+
     const token = await user.generateJWTToken();
     user.password = undefined
-  
+
     res.cookie('token', token, cookieOptions);
-  
+
     res.status(200).json({
       success: true,
       message: 'User LoggedIN Successfully!',
